c692a728cb4a23a646614e73f7dc22dd3459d2ce
Novo <novo@post.bgu.ac.il>
2021-03-29 20:01:56 +0300

    make a first version of the diagram in the classes UserController without ValidateUserLoggin function and the Task class

---
+++ Backend/BuisnessLayer/Task.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend.BuisnessLayer
+{
+    class Task //i think we can make it struct
+    {
+        public int taskId;
+        public DateTime creationTime;
+        public DateTime dueDate;
+        public string title;
+        public string description;
+
+    }
+}
---
+++ Backend/BuisnessLayer/UserController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace IntroSE.Kanban.Backend.BuisnessLayer
+{
+    class UserController
+    {
+        private Dictionary<string,User> users;
+        public UserController(List<DataUser> ADTUsers) // ADTUser is a User from the DataLayer
+        {
+            foreach (DataUser dataUser in ADTUsers)
+            {
+                User user = new User(dataUser);
+                this.users.Add(dataUser.email, user);
+            }
+        }
+        public User Register(string email, int password)
+        {
+            if (EmailExist(email))
+            {
+                throw new Exception("A user already exist with this Email address");
+            }
+
+            return new User(email, password);
+        }
+
+        private bool EmailExist(string email)
+        {
+            bool emailExist = false;
+
+           foreach(KeyValuePair<string,User> valuePair in users)
+            {
+                if (valuePair.Key.Equals(email))
+                {
+                    emailExist = true;
+                    break;
+                }
+            }
+            return emailExist;
+        }
+
+        public User Login(string email, int password)
+        {
+            if (EmailExist(email)) {
+                User user = users[email];
+                if (user.isMyPassword(password)) 
+                {
+                    return user;
+                }
+                    
+            }
+            throw new Exception("Email or Password is invalid");
+        }
+
+       
+
+    }
+}

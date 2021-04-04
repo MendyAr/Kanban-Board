de57385a6a79e5fe75f559f803aa64e3e8380855
Tiefen-boop <roi.tief97@gmail.com>
2021-04-04 21:14:21 +0300

    board foundation

--- Backend/BuisnessLayer/Board.cs
+++ Backend/BuisnessLayer/Board.cs
@@ -8,5 +8,26 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
 {
     class Board
     {
+        //fields
+        private String id;
+        private String name;
+        private String creatorEmail;
+        private int maxBacklog = -1;
+        private int maxInProgress = -1;
+        private int maxDone = -1;
+        private LinkedList<Task> backlog = new LinkedList<Task>;
+        private LinkedList<Task> inProgress= new LinkedList<Task>;
+        private LinkedList<Task> done = new LinkedList<Task>;
+
+        //constructor
+        public Board(String name, String creatorEmail)
+        {
+            this.name = name;
+            this.creatorEmail = creatorEmail;
+            this.id = name + creatorEmail;
+        }
+
+        //methods
+        public void changeNa
     }
 }

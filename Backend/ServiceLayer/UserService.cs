de4dffe9820db7bda5e98cb7e0ec95e41e8caa19
Mendy Aharonian <mendya@post.bgu.ac.il>
2021-04-11 21:54:27 +0300

    adding logger for service

--- Backend/ServiceLayer/UserService.cs
+++ Backend/ServiceLayer/UserService.cs
@@ -9,17 +9,12 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
     class UserService
     {
         private UserController uc = new UserController();
-        public Response Register(string email, string password)
+        internal Response Register(string email, string password)
         {
             throw new NotImplementedException();
         }
 
-        public Response<User> Login(string email, string password)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Response Logout(string email)
+        internal Response<User> Login(string email, string password)
         {
             throw new NotImplementedException();
         }

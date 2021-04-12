4693a6bc19fb91a4373bb427b125d40758960d3b
Novo <novo@post.bgu.ac.il>
2021-04-12 09:33:18 +0300

    Adding validateDescription and validateTitle function

--- Backend/BusinessLayer/Task.cs
+++ Backend/BusinessLayer/Task.cs
@@ -12,6 +12,8 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
 
        public Task(int taskId,DateTime creationTime,  string title, string description, DateTime dueDate)
         {
+            validateTitle(title);
+            validateDescription(description);
             this.taskId = taskId;
             this.creationTime = creationTime;
             this.dueDate = dueDate;
@@ -20,7 +22,32 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
 
             // log.add("create new Task)
         }
+
+        private void validateDescription(string description)
+        {
+            if(description!= null && description.Length > 500)
+            {
+                throw new FormatException("Description max length is 500 characters");
+            }   
+        }
+
         internal int TaskId { get { return taskId; } }
+
+        private void validateTitle(string title)
+        {
+            if(title == null)
+            {
+                throw new ArgumentNullException("Title must not be null");
+            }
+            if (title.Length == 0) 
+            {
+                throw new FormatException("Title cannot be empty");
+            }
+            if (title.Length > 50)
+            {
+                throw new FormatException("Title length cannot be more then 50 characters");
+            }
+        }
 
         internal DateTime CreationTime { get { return creationTime; }}
         internal DateTime DueDate { get { return dueDate; } set { dueDate = value; } }

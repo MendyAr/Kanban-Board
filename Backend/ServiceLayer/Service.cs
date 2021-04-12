a4a97e7e0479e9a38918f2245890a27099790b8d
Mendy Aharonian <mendya@post.bgu.ac.il>
2021-04-12 14:49:57 +0300

    updating logout method in Service

--- Backend/ServiceLayer/Service.cs
+++ Backend/ServiceLayer/Service.cs
@@ -71,7 +71,12 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         /// <returns>A response object. The response should contain a error message in case of an error</returns>
         public Response Logout(string email)
         {
-            if (ConnectedEmail == email)
+            if (email == null)
+            {
+                log.Info("Trying to logout with null email!");
+                return new Response("It's not possible to logout with null email");
+            }
+            else if (ConnectedEmail == email)
             {
                 ConnectedEmail = null;
                 log.Info("logged out successfuly!");

d589d734e665408f3dcdd592c3a6f504b37bec8b
Mendy Aharonian <mendya@post.bgu.ac.il>
2021-04-13 00:58:15 +0300

    adding code to UserController.EmailExist, to Service.Register and adding documatation to Service, UserService, userController, User

--- Backend/ServiceLayer/UserService.cs
+++ Backend/ServiceLayer/UserService.cs
@@ -15,6 +15,11 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
         {
             uc = new UserController();
         }
+
+        ///<summary>Registers a new user to the system.</summary>
+        ///<param name="email">the user e-mail address, used as the username for logging the system.</param>
+        ///<param name="password">the user password.</param>
+        ///<returns cref="Response">The response of the action</returns>
         internal Response Register(string email, string password)
         {
             try
@@ -27,6 +32,13 @@ namespace IntroSE.Kanban.Backend.ServiceLayer
                 return new Response(ex.Message);
             }
         }
+        
+        /// <summary>
+        /// Log in an existing user
+        /// </summary>
+        /// <param name="email">The email address of the user to login</param>
+        /// <param name="password">The password of the user to login</param>
+        /// <returns>A response object with a value set to the user, instead the response should contain a error message in case of an error</returns>
         internal Response<User> Login(string email, string password)
         {
             try

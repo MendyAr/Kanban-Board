471635bd954af1338048b36a4ea053013bc0dbb7
Mendy Aharonian <mendya@post.bgu.ac.il>
2021-04-14 14:51:33 +0300

    moving password check to userController

--- Backend/BusinessLayer/UserController.cs
+++ Backend/BusinessLayer/UserController.cs
@@ -15,6 +15,8 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
         //filed
         
         private Dictionary<string,User> users;
+        private static readonly int PASS_MIN_LENGTH = 4;
+        private static readonly int PASS_MAX_LENGTH = 20;
         private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         
         //constructor
@@ -40,11 +42,12 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
                 log.Info("Trying to register with an existing email!");
                 throw new Exception("A user already exist with this Email address");
             }
+            validatePasswordRules(password);
 
-            User us = new User(email, password);
-            this.users.Add(email, us);
-            log.Info("new User created and added successfully!");
-            return us;
+            User newUser = new User(email, password);
+            this.users.Add(email, newUser);
+            log.Info("A new user created and added successfully!");
+            return newUser;
         }
 
         /// <summary>
@@ -63,9 +66,8 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
             var emailValidator = new EmailAddressAttribute();
             Regex rg = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
             bool validEmail = rg.IsMatch(email);
-            char[] invalidCharacters = { 'א', 'ב', 'ג', 'ד', 'ה', 'ו', 'ז', 'ח', 'ט', 'י', 'כ', 'ל', 'מ', 'נ', 'ס', 'ע', 'פ', 'צ', 'ק', 'ר', 'ש', 'ת', 'ך', 'ם', 'ן', 'ף', 'ץ', '+', '*', '/', '`', '~' };
             var addr = new System.Net.Mail.MailAddress(email);
-            if(!(addr.Address == email & emailValidator.IsValid(email) & validEmail & email.IndexOfAny(invalidCharacters) == -1))
+            if(!(addr.Address == email & emailValidator.IsValid(email) & validEmail))
             {
                 log.Info("Trying to register with invalid email!");
                 throw new Exception("This email address is invalid, please check for spellMistakes");
@@ -104,6 +106,34 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
             }
             log.Info("Failed to login!");
             throw new Exception("Email or Password is invalid");
+        }
+
+        /// <summary>
+        /// check the structure of the password.
+        /// </summary>
+        /// <param name="pass">password to check.</param>
+        /// <exception cref="Exception">thrown when the password doesn't apply the structure rules.</exception>
+        private void validatePasswordRules(string pass)
+        {
+            if (pass == null)   //check null input
+                throw new Exception("password is null");
+
+            if (pass.Length > PASS_MAX_LENGTH | pass.Length < PASS_MIN_LENGTH) //check length
+                throw new Exception("password length is incorrect");
+
+            char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            if (pass.IndexOfAny(numbers) == -1)         // check contain a number
+                throw new Exception("the password doesn't contain a number");
+
+            char[] lowerCase = { 'a','b','c','d','e','f','g','h','i','j','k','l','m',
+                                'n','o','p','q','r','s','t','u','v','w','x','y','z'};
+            if (pass.IndexOfAny(lowerCase) == -1)       // check contain a lower case letter
+                throw new Exception("the password doesn't contain a lowercase letter");
+
+            char[] upperCase = { 'A','B','C','D','E','F','G','H','I','J','K','L','M',
+                                'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+            if (pass.IndexOfAny(upperCase) == -1)       // check contain an upper case letter
+                throw new Exception("the password doesn't contain a upper case letter");
         }
     }
 }

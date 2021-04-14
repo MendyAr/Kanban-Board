6145ffc9b0847e22089385c33c86a352a1cfd3bd
Tiefen-boop <roi.tief97@gmail.com>
2021-04-15 01:40:10 +0300

    cleanup + doc & log for

--- Backend/BusinessLayer/UserController.cs
+++ Backend/BusinessLayer/UserController.cs
@@ -12,16 +12,16 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
 {
     class UserController
     {   
-        //filed
+        //fields
         
-        private Dictionary<string,User> users;
+        private Dictionary<string,User> users; //key - email, value - user of that email
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        //password limiters
         private static readonly int PASS_MIN_LENGTH = 4;
         private static readonly int PASS_MAX_LENGTH = 20;
-        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
         
-        //constructor
-        
-        public UserController() // temprary constructor until we use DataBases
+        //constructors
+        public UserController() 
         {
             users = new Dictionary<string, User>();
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
@@ -37,53 +37,25 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
         ///<exception cref="Exception">thrown when email is null, not in email structure or when user with this email already exist.</exception>
         internal User Register(string email, string password)
         {
-            if (EmailExist(email))
+            if (users.ContainsKey(email))
             {
-                log.Info("Trying to register with an existing email!");
+                log.Warn("FAILED register attempt: '" + email + "' already exists");
                 throw new Exception("A user already exist with this Email address");
             }
-            validatePasswordRules(password);
-
+            try
+            {
+                ValidateEmail(email);
+                validatePasswordRules(password);
+            }
+            catch (Exception e)
+            {
+                log.Info("FAILED register attempt: '" + email + "' " + e.Message);
+                throw new Exception(e.Message);
+            }
             User newUser = new User(email, password);
             this.users.Add(email, newUser);
-            log.Info("A new user created and added successfully!");
+            log.Info("SUCCESSFULLY registered attempt: '" + email + "'");
             return newUser;
-        }
-
-        /// <summary>
-        /// check if the input string match an email structure.
-        /// </summary>
-        /// <param name="email">the input email.</param>
-        ///<exception cref="Exception">thrown when email is null or not in email structure.</exception>
-        private void ValidateEmail(string email)
-        {
-            if (email == null)
-            {
-                log.Info("Tryng to Register with null email!");
-                throw new Exception("Email cannot be null");
-            }
-
-            var emailValidator = new EmailAddressAttribute();
-            Regex rg = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
-            bool validEmail = rg.IsMatch(email);
-            var addr = new System.Net.Mail.MailAddress(email);
-            if(!(addr.Address == email & emailValidator.IsValid(email) & validEmail))
-            {
-                log.Info("Trying to register with invalid email!");
-                throw new Exception("This email address is invalid, please check for spellMistakes");
-            }
-        }
-
-        /// <summary>
-        /// check if a user with the same email address exist in the system.
-        /// </summary>
-        /// <param name="email">the input email need to be checked.</param>
-        /// <returns>true/false accordingly.</returns>
-        /// ///<exception cref="Exception">thrown when email is null or not in email structure.</exception>
-        internal bool EmailExist(string email)
-        {
-            ValidateEmail(email); 
-            return users.ContainsKey(email);
         }
 
         /// <summary>
@@ -95,7 +67,7 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
         /// <exception cref="Exception">thrown when a user with this email doesn't exist or when the password is incorrect.</exception>
         internal User Login(string email, string password)
         {
-            if (EmailExist(email)) {
+            if (users.ContainsKey(email)) {
                 User user = users[email];
                 if (user.validatePassword(password)) 
                 {
@@ -109,6 +81,28 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
         }
 
         /// <summary>
+        /// check if the input string match an email structure.
+        /// </summary>
+        /// <param name="email">the input email.</param>
+        ///<exception cref="Exception">thrown when email is null or not in email structure.</exception>
+        private void ValidateEmail(string email)
+        {
+            if (email == null)
+            {
+                throw new Exception("Email cannot be null");
+            }
+
+            var emailValidator = new EmailAddressAttribute();
+            Regex rg = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+            bool validEmail = rg.IsMatch(email);
+            var addr = new System.Net.Mail.MailAddress(email);
+            if(!(addr.Address == email & emailValidator.IsValid(email) & validEmail))
+            {
+                throw new Exception("This email address is invalid, please check for spellMistakes");
+            }
+        }
+
+        /// <summary>
         /// check the structure of the password.
         /// </summary>
         /// <param name="pass">password to check.</param>
@@ -118,8 +112,12 @@ namespace IntroSE.Kanban.Backend.BuisnessLayer
             if (pass == null)   //check null input
                 throw new Exception("password is null");
 
-            if (pass.Length > PASS_MAX_LENGTH | pass.Length < PASS_MIN_LENGTH) //check length
-                throw new Exception("password length is incorrect");
+            //check length
+            if (pass.Length < PASS_MIN_LENGTH)
+                throw new Exception("password too short");
+            if (pass.Length > PASS_MAX_LENGTH ) 
+                throw new Exception("password too long");
+
 
             char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
             if (pass.IndexOfAny(numbers) == -1)         // check contain a number

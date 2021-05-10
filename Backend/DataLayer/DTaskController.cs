9e0cd15c289d859ab244fde3526c2e4fcdd88e17
Novo <novo@post.bgu.ac.il>
2021-05-10 23:38:22 +0300

    implement second select

--- Backend/DataLayer/DTaskController.cs
+++ Backend/DataLayer/DTaskController.cs
@@ -10,14 +10,17 @@ namespace IntroSE.Kanban.Backend.DataLayer
         {}
         protected override DTO ConvertReaderToObject(SQLiteDataReader reader)
         {
-            int taskId = reader.GetInt32(0);
-            DateTime creationTime = DateTime.Parse(reader.GetString(1));
-            string title = reader.GetString(2);
-            string description = reader.GetString(3);
-            DateTime dueDate = DateTime.Parse(reader.GetString(4));
-            string assignee = reader.GetString(5);
+            int taskId = reader.GetInt32(1);
+            DateTime creationTime = DateTime.Parse(reader.GetString(2));
+            string title = reader.GetString(3);
+            string description = reader.GetString(4);
+            DateTime dueDate = DateTime.Parse(reader.GetString(5));
+            string assignee = reader.GetString(6);
+            int ordinal = reader.GetInt32(7);
+            string boardCreator = reader.GetString(8);
+            string boardName = reader.GetString(9);
 
-            DTask result = new DTask(taskId,creationTime,title,description,dueDate,assignee);
+            DTask result = new DTask(taskId,creationTime,title,description,dueDate,assignee,ordinal,boardCreator,boardName);
             return result;
         }
 
@@ -55,19 +58,21 @@ namespace IntroSE.Kanban.Backend.DataLayer
             }      
         }
 
-        public List<DTO> Select(string creator,string boardName,int taskId)
+        public List<DTO> Select(string boardCreator,string boardName)
 
         {
-            string ID =  taskId + creator + boardName;
             List<DTO> results = new List<DTO>();
             using (var connection = new SQLiteConnection(_connectionString))
             {
                 SQLiteCommand command = new SQLiteCommand(null, connection);
-                command.CommandText = $"select * from {_tableName};";
+                command.CommandText = $"select * from {_tableName} WHERE (BoardCreator = @{boardCreator} AND BoardName = @{boardName})";
+
                 SQLiteDataReader dataReader = null;
                 try
                 {
                     connection.Open();
+                    command.Parameters.Add(new SQLiteParameter(boardCreator,boardCreator));
+                    command.Parameters.Add(new SQLiteParameter(boardName, boardName));
                     dataReader = command.ExecuteReader();
 
                     while (dataReader.Read())

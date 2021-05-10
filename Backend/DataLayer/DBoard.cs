0204005203626ce04003a0bb97eeb63e05709471
Novo <novo@post.bgu.ac.il>
2021-05-10 12:40:51 +0300

    delete limits

--- Backend/DataLayer/DBoard.cs
+++ Backend/DataLayer/DBoard.cs
@@ -20,58 +20,13 @@ namespace IntroSE.Kanban.Backend.DataLayer
                 _boardName = value;
             } }
 
-
-        private int _limitColumn1;
-        public int LimitColumn1
-        {
-            get => _limitColumn1; set
-            {
-                if (Persist)
-                {
-                    Update("LimitColumn1", value);
-                }
-                _limitColumn1 = value;
-            }
-        }
-
-
-        private int _limitColumn2;
-        public int LimitColumn2
-        {
-            get => _limitColumn2; set
-            {
-                if (Persist)
-                {
-                    Update("LimitColumn2", value);
-                }
-                _limitColumn2 = value;
-            }
-        }
-
-
-        private int _limitColumn3;
-        public int LimitColumn3
-        {
-            get => _limitColumn3; set
-            {
-                if (Persist)
-                {
-                    Update("LimitColumn3", value);
-                }
-                _limitColumn3 = value;
-            }
-        }
-
         private DColumn[] columns;
         public DColumn[] Columns { get => columns; }
 
-        public DBoard (string creator, string boardName,int limitColumn1, int limitColumn2, int limitColumn3) : base(new DBoardController(),creator + boardName)
+        public DBoard (string creator, string boardName) : base(new DBoardController(),creator + boardName)
         {
             this._creator = creator;
             this._boardName = boardName;
-            _limitColumn1 = limitColumn1;
-            _limitColumn2 = limitColumn2;
-            _limitColumn3 = limitColumn3;
             columns = new DColumn[3];
         }
     }

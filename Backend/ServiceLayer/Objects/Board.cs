ff481502028a03b768fb1b8ef0df078c5c1e33b1
RoiTief <roitief@post.bgu.ac.il>
2021-05-11 19:02:13 +0300

    Implemented class

---
+++ Backend/ServiceLayer/Objects/Board.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BBoard = IntroSE.Kanban.Backend.BuisnessLayer.Board;
+
+namespace IntroSE.Kanban.Backend.ServiceLayer.Objects
+{
+    public struct Board
+    {
+        public readonly string CreatorEmail;
+        public readonly string Name;
+        public readonly IList<string> Columns;
+        public readonly int TaskCounter;
+
+        internal Board(string creatorEmail, string boardName, IList<string> columnNames, int taskCounter)
+        {
+            CreatorEmail = creatorEmail;
+            Name = boardName;
+            Columns = new List<string>(columnNames);
+            TaskCounter = taskCounter;
+        }
+
+        internal Board(string creatorEmail, string boardName, BBoard bBoard)
+        {
+            CreatorEmail = creatorEmail;
+            Name = boardName;
+            Columns = bBoard.Columns;
+            TaskCounter = bBoard.TaskIdCounter;
+        }
+    }
+}

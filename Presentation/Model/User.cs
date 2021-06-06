e832cff90482e3b59432ccd377d349d160656b05
Novo <novo@post.bgu.ac.il>
2021-06-07 00:59:35 +0300

    initiolaze

---
+++ Presentation/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Model
+{
+    public class User
+    {
+
+        public User(IntroSE.Kanban.Backend.ServiceLayer.User value)
+        {
+           
+        }
+    }
+}

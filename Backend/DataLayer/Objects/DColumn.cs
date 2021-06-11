57bf7ff5c0fec21501b1ed88dc062adc94ef0c7d
Novo <novo@post.bgu.ac.il>
2021-06-11 17:56:45 +0300

    add change ordinal

--- Backend/DataLayer/Objects/DColumn.cs
+++ Backend/DataLayer/Objects/DColumn.cs
@@ -22,14 +22,18 @@ namespace IntroSE.Kanban.Backend.DataLayer
         private const string COL_ORDINAL = "Ordinal";
         private const string TASK_TABLE_NAME = "Task";
 
-        internal string Name { get => _name; 
-            set {
+        internal string Name
+        {
+            get => _name;
+            set
+            {
                 if (Persist)
                 {
                     Update(COL_NAME, value);
                 }
                 _name = value;
-            } }
+            }
+        }
 
         internal string CreatorEmail
         {
@@ -66,21 +70,21 @@ namespace IntroSE.Kanban.Backend.DataLayer
                 _limit = value;
             }
         }
-        internal int Ordinal { get => _ordinal; 
-            set {
+        internal int Ordinal
+        {
+            get => _ordinal;
+            set
+            {
                 if (Persist)
                 {
-                    
+
                     Update(COL_ORDINAL, value);
                     Update(COL_ID, CreatorEmail + BoardName + value);
                 }
-                foreach (DTask task in _tasks)
-                {
-                    task.Ordinal = value;
-                }
+                ChangeTaskOrdinal(value);
                 _ordinal = value;
-                }
-        } 
+            }
+        }
 
         internal List<DTask> Tasks
         {
@@ -93,7 +97,7 @@ namespace IntroSE.Kanban.Backend.DataLayer
 
         // constructor
 
-        internal DColumn(string name,string creatorEmail, string boardName, int ordinal, int limit) : base(creatorEmail + boardName + ordinal, "Column")
+        internal DColumn(string name, string creatorEmail, string boardName, int ordinal, int limit) : base(creatorEmail + boardName + ordinal, "Column")
         {
             _creatorEmail = creatorEmail;
             _boardName = boardName;
@@ -133,7 +137,7 @@ namespace IntroSE.Kanban.Backend.DataLayer
                 };
                 try
                 {
-                    command.Parameters.Add(new SQLiteParameter(COL_ORDINAL, Ordinal-1));
+                    command.Parameters.Add(new SQLiteParameter(COL_ORDINAL, Ordinal - 1));
                     command.Parameters.Add(new SQLiteParameter(COL_BOARD_NAME, BoardName));
                     command.Parameters.Add(new SQLiteParameter(COL_CREATOR_EMAIL, CreatorEmail));
                     command.Parameters.Add(new SQLiteParameter(thisColumn, Ordinal));
@@ -166,6 +170,49 @@ namespace IntroSE.Kanban.Backend.DataLayer
                 dTask.ReduceOrdinal();
             }
 
+        }
+
+        internal void ChangeTaskOrdinal(int newOrdinal)
+        {
+            bool errorOcurred = false;
+            int res = -1;
+            string thisColumn = "ThisColumn";
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                SQLiteCommand command = new SQLiteCommand
+                {
+                    Connection = connection,
+                    CommandText = $"update {TASK_TABLE_NAME} set [{COL_ORDINAL}]=@{COL_ORDINAL} where ({COL_BOARD_NAME}=@{COL_BOARD_NAME} AND {COL_CREATOR_EMAIL}=@{COL_CREATOR_EMAIL} AND {COL_ORDINAL}=@{thisColumn}) "
+                };
+                try
+                {
+                    command.Parameters.Add(new SQLiteParameter(COL_ORDINAL, newOrdinal));
+                    command.Parameters.Add(new SQLiteParameter(COL_BOARD_NAME, BoardName));
+                    command.Parameters.Add(new SQLiteParameter(COL_CREATOR_EMAIL, CreatorEmail));
+                    command.Parameters.Add(new SQLiteParameter(thisColumn, Ordinal));
+                    connection.Open();
+                    res = command.ExecuteNonQuery();
+                }
+                catch (Exception e)
+                {
+                    errorOcurred = true;
+                    log.Error($"Failed to update data in the DB, tried command: {command.CommandText},\n" +
+                         $"the SQLite exception massage was: {e.Message}");
+                }
+                finally
+                {
+                    command.Dispose();
+                    connection.Close();
+                    if (errorOcurred)
+                        throw new InvalidOperationException();
+                }
+
+            }
+            if (res <= 0)
+            {
+                log.Error($"SQLite Update query in table '{_tableName}' returned {res}.");
+                throw new Exception("The data didn't update correctly");
+            }
         }
     }
 }

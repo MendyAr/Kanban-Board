afa8b6524ebac18a281712f9b31a4a595e84ae42
Novo <novo@post.bgu.ac.il>
2021-06-11 16:38:57 +0300

    imlement UserView

---
+++ Presentation/View/BoardView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Frontend.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Frontend.View
+{
+    /// <summary>
+    /// Interaction logic for BoardView.xaml
+    /// </summary>
+    public partial class BoardView : Window
+    {
+        public BoardView(Board board)
+        {
+            InitializeComponent();
+        }
+    }
+}

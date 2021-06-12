36a6f174b88a15076c13a1358fcb46c3e3b717cf
RoiTief <roitief@post.bgu.ac.il>
2021-06-13 00:49:55 +0300

    work

--- Frontend/ViewModel/TaskViewModel.cs
+++ Frontend/ViewModel/TaskViewModel.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace IntroSE.Kanban.Frontend.ViewModel
 {
-    class TaskViewModel
+    class TaskViewModel : ViewModelObject
     {
+        private string _userEmail;
+        private string _boardCreator;
+        private string _boardName;
+        private int _columnOrdinal;
+        private int _taskID;
+        private string _title;
+        private string _assignee;
+        private string _description;
+        private string _creationDate;
+        private DateTime _dueDate;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                try
+                {
+                    UpdateTitle()
+                }
+            }
+        }
     }
 }

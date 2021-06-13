4182d6bdee14d4d73107fc35d417c0cd76536d4b
RoiTief <roitief@post.bgu.ac.il>
2021-06-13 17:32:45 +0300

    added NotifiableModelObject

---
+++ Frontend/Model/NotifiableModelObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Frontend.Model
+{
+    public abstract class NotifiableModelObject : NotifiableObject
+    {
+        public BackendController Controller { get; private set; }
+        protected NotifiableModelObject(BackendController controller)
+        {
+            this.Controller = controller;
+        }
+    }
+}
--- Frontend/Model/Task.cs
+++ Frontend/Model/Task.cs
@@ -8,5 +8,13 @@ namespace IntroSE.Kanban.Frontend.Model
 {
     internal class Task
     {
+        public readonly int Id;
+        public readonly DateTime CreationTime;
+        public readonly string Title;
+        public readonly string Description;
+        public readonly DateTime DueDate;
+        public readonly string emailAssignee;
+
+
     }
 }
